0bcfd44b12c9a8f989f1ce1bc4b9eef699f71e01
martendo <51766598+martendo@users.noreply.github.com>
2024-04-10 08:42:23 -0400

    Add GifPixelActor

---
+++ GifPixelActor.java
@@ -0,0 +1,36 @@
+import greenfoot.*;
+
+/**
+ * A PixelActor subclass that simplifies the use of a GifImage as its image.
+ * <p>
+ * Subclasses of GifPixelActor should call the {@link #updateImage} method every
+ * act.
+ *
+ * @author Martin Baldwin
+ * @version April 2024
+ */
+public abstract class GifPixelActor extends PixelActor {
+    private final GifImage gif;
+
+    /**
+     * Creates a GifPixelActor using the given GifImage as its animated image.
+     *
+     * @param gif the GifImage to use to animate the actor's image
+     */
+    public GifPixelActor(GifImage gif) {
+        super(gif.getCurrentImage());
+        this.gif = gif;
+    }
+
+    /**
+     * Updates this actor's image according to its GifImage when appropriate.
+     * <p>
+     * Subclasses should call this method every act.
+     */
+    public void updateImage() {
+        GreenfootImage frame = gif.getCurrentImage();
+        if (getOriginalImage() != frame) {
+            setImage(frame);
+        }
+    }
+}
