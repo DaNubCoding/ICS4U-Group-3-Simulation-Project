37eb980cf408a015dc11e0c7abc7c2283d822fc7
martendo <51766598+martendo@users.noreply.github.com>
2024-04-18 19:38:20 -0400

    Fix tuna evolution

--- Tuna.java
+++ Tuna.java
@@ -15,7 +15,7 @@ public class Tuna extends TierOneFish
     //sets this tier one fish's settings to basic settings, with specified body image and egg color
     static {
         settings = new FishSettings();
-        initialiseTierOneFishSettings(settings, "fishes/tuna.png", Egg.EggColor.BLUE, Swordfish.class);
+        initialiseTierOneFishSettings(settings, "fishes/tuna.png", Egg.EggColor.BLUE, Flyingfish.class);
     }
 
     public Tuna(int evoPoints, FishFeature... features) {
