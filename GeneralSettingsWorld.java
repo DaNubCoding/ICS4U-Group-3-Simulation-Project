c7b9d89589e74f54b0c56db5800ad80b60868655
DaNubCoding <danubcoding@gmail.com>
2024-04-22 22:24:54 -0400

    make SettingsWorld into abstract superclass

--- GeneralSettingsWorld.java
+++ GeneralSettingsWorld.java
@@ -10,81 +10,26 @@ import java.util.LinkedHashMap;
  * @author Matthew Li
  * @version April 2024
  */
-public class GeneralSettingsWorld extends PixelWorld {
-    private static final GreenfootImage background = new GreenfootImage("settings_world_background.png");
-
-    private boolean keyPressed = true;
-
-    private static UserSettings userSettings = new UserSettings();
-
+public class GeneralSettingsWorld extends SettingsWorld {
     /** @see UserSettings */
     private UserSettingSlider<Integer> expThresholdSlider;
     private UserSettingSlider<Integer> eggSpawnAmountSlider;
     private UserSettingSlider<Integer> numOfStartFishSlider;
 
-    private LinkedHashMap<String, Slider<?>> sliders;
-
     public GeneralSettingsWorld() {
-        super(250, 160);
-
-        constructSliders();
-
-        int align = getWidth() / 2;
-        int i = 0;
-        for (Map.Entry<String, Slider<?>> slider : sliders.entrySet()) {
-            Text text = new Text(slider.getKey(), Text.AnchorX.RIGHT, Text.AnchorY.CENTER);
-            addObject(text, align - 4, 10 + i * 18);
-            addObject(slider.getValue(), align + 4, 10 + i++ * 18);
-        }
-
-        addObject(new Button("Start!", () -> triggerFadeOut(0.02)), getWidth() / 2, 140);
-
-        triggerFadeIn(0.02);
-        render();
+        super();
     }
 
-    /**
-     * Initialize the sliders.
-     */
-    private void constructSliders() {
-        /** @see UserSettings */
+    @Override
+    public void constructSliders() {
+        UserSettings userSettings = getUserSettings();
+
         expThresholdSlider = new UserSettingSlider<Integer>(1, 5, 1, new Color(76, 45, 23), userSettings::setEvoPointThreshold);
         eggSpawnAmountSlider = new UserSettingSlider<Integer>(1, 5, 3, new Color(76, 45, 23), userSettings::setEggSpawnAmount);
         numOfStartFishSlider = new UserSettingSlider<Integer>(1, 10, 1, new Color(76, 45, 23), userSettings::setNumOfStartFish);
 
-        sliders = new LinkedHashMap<String, Slider<?>>();
-
-        sliders.put("EXP Threshold", expThresholdSlider);
-        sliders.put("Max # of Eggs", eggSpawnAmountSlider);
-        sliders.put("# of Starting Fish", numOfStartFishSlider);
-    }
-
-    @Override
-    public void act() {
-        render();
-        Timer.incrementAct();
-
-        if (Greenfoot.isKeyDown("Enter") && !keyPressed) {
-            triggerFadeOut(0.02);
-        }
-        keyPressed = Greenfoot.isKeyDown("Enter");
-
-        if (isFadeOutComplete()) {
-            Greenfoot.setWorld(new SimulationWorld(userSettings));
-        }
-    }
-
-    /**
-     * Updates the display image of this world.
-     */
-    private void render() {
-        GreenfootImage canvas = getCanvas();
-        // Draw the background
-        canvas.drawImage(background, 0, 0);
-        // Draw actors
-        renderPixelActors();
-
-        // Display new canvas image
-        updateImage();
+        addSlider("EXP Threshold", expThresholdSlider);
+        addSlider("Max # of Eggs", eggSpawnAmountSlider);
+        addSlider("# of Starting Fish", numOfStartFishSlider);
     }
 }
---
+++ SettingsWorld.java
@@ -0,0 +1,107 @@
+import greenfoot.*;
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+/**
+ * The superclass to all worlds that lets the user initialize settings of the simulation.
+ *
+ * @author Andrew Wang
+ * @version April 2024
+ */
+public abstract class SettingsWorld extends PixelWorld {
+    private static final GreenfootImage background = new GreenfootImage("settings_world_background.png");
+
+    private boolean keyPressed = true;
+
+    private UserSettings userSettings;
+    private LinkedHashMap<String, Slider<?>> sliders;
+
+    /**
+     * Create a SettingsWorld and supply a partially initialized UserSettings
+     * from the previous SettingsWorld.
+     *
+     * @param userSettings The UserSettings object from the previous SettingsWorld
+     */
+    public SettingsWorld(UserSettings userSettings) {
+        super(250, 160);
+        this.userSettings = userSettings;
+        this.sliders = new LinkedHashMap<String, Slider<?>>();
+        constructSliders();
+
+        int align = getWidth() / 2;
+        int i = 0;
+        for (Map.Entry<String, Slider<?>> slider : sliders.entrySet()) {
+            Text text = new Text(slider.getKey(), Text.AnchorX.RIGHT, Text.AnchorY.CENTER);
+            addObject(text, align - 4, 10 + i * 18);
+            addObject(slider.getValue(), align + 4, 10 + i++ * 18);
+        }
+
+        addObject(new Button("Start!", () -> triggerFadeOut(0.02)), getWidth() / 2, 140);
+
+        triggerFadeIn(0.02);
+        render();
+    }
+
+    /**
+     * Create a SettingsWorld without supplying a partially initialized
+     * UserSettings from the previous SettingsWorld.
+     */
+    public SettingsWorld() {
+        this(new UserSettings());
+    }
+
+    /**
+     * Initialize the sliders.
+     */
+    public abstract void constructSliders();
+
+    @Override
+    public void act() {
+        render();
+        Timer.incrementAct();
+
+        if (Greenfoot.isKeyDown("Enter") && !keyPressed) {
+            triggerFadeOut(0.02);
+        }
+        keyPressed = Greenfoot.isKeyDown("Enter");
+
+        if (isFadeOutComplete()) {
+            Greenfoot.setWorld(new SimulationWorld(userSettings));
+        }
+    }
+
+    /**
+     * Updates the display image of this world.
+     */
+    private void render() {
+        GreenfootImage canvas = getCanvas();
+        // Draw the background
+        canvas.drawImage(background, 0, 0);
+        // Draw actors
+        renderPixelActors();
+
+        // Display new canvas image
+        updateImage();
+    }
+
+    /**
+     * Get the UserSettings object of this SettingsWorld, containing all settings
+     * set in this world and the ones before it.
+     *
+     * @return The UserSettings object of this SettingsWorld
+     */
+    public UserSettings getUserSettings() {
+        return userSettings;
+    }
+
+    /**
+     * Add a new Slider to the list of Sliders that will be shown in this
+     * SettingsWorld.
+     *
+     * @param label The label displayed next to the Slider
+     * @param slider The Slider object
+     */
+    public void addSlider(String label, Slider<?> slider) {
+        sliders.put(label, slider);
+    }
+}
