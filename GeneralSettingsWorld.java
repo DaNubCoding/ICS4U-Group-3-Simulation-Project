2e5058beb2c749808621fb5eb61e5a11e3ff72a9
martendo <51766598+martendo@users.noreply.github.com>
2024-04-24 20:01:12 -0400

    Add settings music

--- GeneralSettingsWorld.java
+++ GeneralSettingsWorld.java
@@ -40,8 +40,7 @@ public class GeneralSettingsWorld extends SettingsWorld {
         addObject(tunaButton, midx + 40, top + 35);
         addObject(new UserSettingSlider<Integer>(0, 10, 1, 30, new Color(46, 92, 107), userSettings::setTunaCount), midx + 25, top + 55);
 
-        // TODO: add music
-        Music.stop();
+        Music.play("settings_music.wav");
     }
 
     @Override
